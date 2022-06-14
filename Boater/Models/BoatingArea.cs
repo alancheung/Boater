773329aedf657ce0279426786df961040b96293b
Alan <cheung.alan94@gmail.com>
2022-06-13 21:43:21 -0400

    Add boating area configuration file and fix properties

--- Boater/Models/BoatingArea.cs
+++ Boater/Models/BoatingArea.cs
@@ -1,14 +1,37 @@
-﻿using System;
+﻿using Newtonsoft.Json;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Boater.Models
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class BoatingArea
     {
+        /// <summary>
+        /// Official title to display on the reading panel header.
+        /// </summary>
         public string Title { get; set; }
+
+        /// <summary>
+        /// The URL to obtain the RSS feed of station readings.
+        /// </summary>
+        public string RssUrl { get; set; }
+
+        /// <summary>
+        /// The URL to obtain text updates to display in the left panel.
+        /// </summary>
+        public string TextUrl { get; set; }
+
+        /// <summary>
+        /// The maximum distance from the station to still allow results.
+        /// </summary>
+        public int MaxRange { get; set; }
+
+        /// <summary>
+        /// The data from each station
+        /// </summary>
+        [JsonIgnore]
         public List<StationSource> Stations { get; private set; }
     }
 }
