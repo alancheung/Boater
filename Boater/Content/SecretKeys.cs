b036d2715f542421743dfc5d3031a4ee3a849264
Alan <cheung.alan94@gmail.com>
2022-06-14 20:35:56 -0400

    Add OpenWeatherAPIKey

---
+++ Boater/Content/SecretKeys.cs
@@ -0,0 +1,10 @@
+﻿namespace Boater.Content
+{
+    public partial class SecretKeys
+    {
+        /// <summary>
+        /// The API key for OpenWeatherMap.
+        /// </summary>
+        public static string OpenWeatherAPI = "";
+    }
+}
