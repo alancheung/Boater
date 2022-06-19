58f1752e731287c163e5fb12e4bec77c2b4ca94c
achippyland <cheung.alan94@gmail.com>
2022-06-19 17:16:42 -0400

    Round humidity

--- Boater/Services/OpenWeatherMapClient.cs
+++ Boater/Services/OpenWeatherMapClient.cs
@@ -80,6 +80,7 @@ namespace Boater.Services
                     result.Item.Temp = Math.Round(result.Item.Temp, 0);
                     result.Item.TempMax = Math.Round(result.Item.TempMax, 0);
                     result.Item.TempMin = Math.Round(result.Item.TempMin, 0);
+                    result.Item.Humidity = Math.Round(result.Item.Humidity, 0);
                 }
                 else
                 {
@@ -143,6 +144,7 @@ namespace Boater.Services
                         f.Temp = Math.Round(f.Temp, 0);
                         f.TempMax = Math.Round(f.TempMax, 0);
                         f.TempMin = Math.Round(f.TempMin, 0);
+                        f.Humidity = Math.Round(f.Humidity, 0);
                     }
                 }
                 else
